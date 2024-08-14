1f9874e2cfdb3e5b7d25c8b803030aea5987a52c
gladiator <139589392+gladiatorpanda@users.noreply.github.com>
2024-08-14 22:33:07 +0530

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,9 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-
-import './globals.css'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins',
-})
-
-export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
-  icons: {
-    icon: '/assets/images/logo.svg'
-  }
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={poppins.variable}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
   )
 }
+
+export default Layout
